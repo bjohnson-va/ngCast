9974f0b8e37f2bb074cb1aeb00f40fcc1c603c9d
[Vendasta] Brad Johnson <bjohnson@vendasta.com>
2019-01-16 15:24:45 -0600

    Fix module

--- ng.cast.module.ts
+++ ng.cast.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material';
 import { NgCastComponent } from './ng-cast.component';
 import { NgCastService } from './shared/ng-cast.service';
 
 @NgModule({
   imports: [
     CommonModule,
-      MaterialIconModule,
+      MatIconModule,
   ],
   exports: [NgCastComponent],
   providers: [NgCastService],
